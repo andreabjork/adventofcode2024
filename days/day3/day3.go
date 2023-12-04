d47d43220311a13f506bf85a333be69db7e497e8
Andrea Bjoernsdottir <andreabjork43@gmail.com>
2023-12-04 12:54:33 +0100

    day 3

--- days/day3/day3.go
+++ days/day3/day3.go
@@ -1,7 +1,7 @@
 package day3
 
 import (
-  "adventofcode/m/v2/util"
+	"adventofcode/m/v2/util"
 	"fmt"
 	"io"
 	"strconv"
@@ -9,43 +9,65 @@ import (
 
 func Day3(inputFile string, part int) {
 	if part == 0 {
-		fmt.Printf("Sum of parts: %d\n", sumParts(inputFile))
+    sum, _ := partsAndGears(inputFile)
+		fmt.Printf("Sum of parts: %d\n", sum)
 	} else {
-		fmt.Printf("Sum of priorities of all groups: %d\n", sumParts(inputFile))
+    _, gears := partsAndGears(inputFile)
+		fmt.Printf("Gear values: %d\n", multiply(gears))
 	}
 }
 
-func sumParts(inputFile string) int { 
+type Part struct {
+  value   int
+  length  int
+  isValid  bool
+  i       int
+  j       int
+}
+
+type Gear struct {
+  typ       rune 
+  adjacents map[*Part]bool
+}
+
+func partsAndGears(inputFile string) (int, []*Gear) { 
   ls := util.Reader(inputFile)
 
-  var marked map[int]map[int]bool = make(map[int]map[int]bool) // marked[i][j] = true if (i,j) is adjacent to a symbol
-  var numbers map[int]map[int][]int = make(map[int]map[int][]int) // numbers[i][j] = x,d where x starts at position (i,j) and x is d digits
+  var marked map[int]map[int][]*Gear = make(map[int]map[int][]*Gear) // marked[i][j] = true if (i,j) is adjacent to a symbol
+  var gears []*Gear = []*Gear{}
+  var parts []*Part = []*Part{} 
+
   r, sz, err := ls.ReadRune()
   
   var i, j int = 0, 0
   var num []rune
   for err != io.EOF && sz > 0 {
     if r == '\n' {
-      addNumber(numbers, num, i, j)
+      parts = addPart(parts, num, i, j)
       num = []rune{}
       i++
       j = -1 
     } else if r == '.' {
-      addNumber(numbers, num, i, j)
+      parts = addPart(parts, num, i, j)
       num = []rune{}
     } else if r >= '0' && r <= '9' {
       num = append(num, r)
     } else { // Symbols
-      addNumber(numbers, num, i, j)
+      parts = addPart(parts, num, i, j)
       num = []rune{}
 
+      g := &Gear{r, make(map[*Part]bool)}
       for ii := util.Max(0, i-1); ii <= i+1; ii++ {
         for jj := util.Max(0, j-1); jj <= j+1; jj++ {
           if marked[ii] == nil {
-            marked[ii] = make(map[int]bool)
+            marked[ii] = make(map[int][]*Gear)
           }
-          marked[ii][jj] = true
+          marked[ii][jj] = append(marked[ii][jj], g)
         }
+      }
+      // Track * gears
+      if r == '*' {
+        gears = append(gears, g)
       }
     }
 
@@ -53,31 +75,43 @@ func sumParts(inputFile string) int {
     j++
   }
 
-  var parts = []int{}
+  // Find which parts are valid (adjacent to gears) and mark the part on the gear
   var sum = 0
-  var found bool = false
-  for i, m := range numbers {
-    for j, mm := range m {
-      found = false
-      for d := 0; d < mm[1]; d++ {
-        if marked[i][j+d] {
-          parts = append(parts, mm[0])
-          sum += mm[0]
-          found = true
-          break
+  for _, n := range parts {
+    for d := 0; d < n.length; d++ {
+        if gs, ok := marked[n.i][n.j+d]; ok && len(gs) > 0 {
+          for _, g := range gs {
+            g.adjacents[n] = true
+          }
+          n.isValid = true
         }
       }
+    if n.isValid {
+        sum += n.value
     }
   }
-  return sum 
+  return sum, gears
 }
 
-func addNumber(numbers map[int]map[int][]int, num []rune, i int, j int) {
+func multiply(gears []*Gear) int {
+  sum := 0
+  for _, g := range gears {
+    if g.typ == '*' && len(g.adjacents) > 1 {
+      mult := 1
+      for adj, _ := range g.adjacents {
+        mult = mult*adj.value
+      }
+      sum += mult
+    }
+  }
+
+  return sum
+}
+
+func addPart(parts []*Part, num []rune, i int, j int) []*Part {
   if len(num) > 0 {
     x, _ := strconv.Atoi(string(num))
-    if numbers[i] == nil { 
-      numbers[i] = make(map[int][]int)
-    }
-    numbers[i][j-len(num)] = []int{x,len(num)}
+    parts = append(parts, &Part{x,len(num),false, i, j-len(num)})
   }
+  return parts 
 }
